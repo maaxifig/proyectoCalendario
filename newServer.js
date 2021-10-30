b9900ac5085ab1af089d44f5030600be054d36f7
Ali Ahmadian <ali.ahmadian@mercadolibre.com>
2021-10-30 14:33:35 -0300

    post request added in newServer.js

--- newServer.js
+++ newServer.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     {id: 1, name: 'course1'},
     {id: 2, name: 'course2'},
@@ -15,9 +17,51 @@ app.get('/', (req, res) => {
 
 });
 
+
 app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
+
+app.post('/api/courses', (req, res) => {
+
+    if (!req.body.name || req.body.name.length < 3) {
+        // 400 Bad request
+        res.status(400).send('Name is required and should be minimum 3 characters!');
+        return;
+    }
+
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    //look up the courses , if not exisiting , return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found!');
+
+
+    // Validate
+    // If invalid , return 400 - Bad request
+    if (!req.body.name || req.body.name.length < 3) {
+            // 400 Bad request
+            res.status(400).send('Name is required and should be minimum 3 characters!');
+            return;
+        }
+
+    // Update course
+    // Return the updated course
+    course.name = req.body.name;
+    res.send(course);
+});
+
+
+
+
+
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
