998f437186cd16bbc35eebdc7a508daf5c47e1c4
maaxifig <maxifigueroa2012@gmail.com>
2021-10-30 15:34:07 -0300

    Se agrega conexion a DB y algunos endpoints

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ var express = require('express');
 // Se invoca la función (de la variable express) y se almacena en la variable app.
 var app = express();
 
+var mysql = require('mysql');
+
+var conexion = mysql.createConnection({
+
+  host: 'localhost',
+  database: 'sistemaReservas',
+  user: 'root',
+  password: 'Loreto18'
+
+});
+
+conexion.connect(function(error){
+  if(error){
+    throw error;
+  }else{
+    console.log('Conexion exitosa');
+  }
+})
+
 // Define el home de la página y que función se va a ejecutar.
 // La función tiene como parámetro el request y el response.
 app.get('/', function (req, res) {
@@ -14,6 +33,35 @@ app.get('/cursos', function (req, res) {
   res.send('Estos son los cursos');
   console.log("Página de cursos");
 })
+
+/*app.get('/users', (request, response) => {
+  pool.query('SELECT * FROM Usuario', (error, result) => {
+      if (error) throw error;
+
+      response.send(result);
+  });
+});*/
+
+app.get('/getAllUsers', (req, res) => {
+  conexion.query('SELECT * FROM Usuario', (err, rows, fields) => {
+    if(!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  });  
+});
+
+app.get('/getUserById/:id', (req, res) => {
+  const { id } = req.params
+  conexion.query('SELECT * FROM Usuario WHERE userId = ?', [id], (err, rows, fields) => {
+    if(!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  });  
+});
 
 // Correr el servidor con el puerto 8989.
 app.listen(5000);
